ad1b4a0b72cb2e49374bd6e2ffdeafbbba6f2694
Andreas Katzian <andreas.katzian@jademind.com>
2026-02-26 11:45:16 +0100

    Release v0.1.8

--- Formula/pi-statusbar.rb
+++ Formula/pi-statusbar.rb
@@ -1,10 +1,9 @@
 class PiStatusbar < Formula
   desc "Pi macOS status bar app with local daemon and session controls"
   homepage "https://github.com/jademind/pi-statusbar"
-  url "https://github.com/jademind/pi-statusbar/archive/refs/tags/v0.1.7.tar.gz"
+  url "https://github.com/jademind/pi-statusbar/archive/refs/tags/v0.1.8.tar.gz"
   sha256 "29a8c75968105e7b0aa0b5f72e67c5ad1de6c033c60e727031b189e80fdd0117"
-  version "0.1.7"
-  revision 1
+  version "0.1.8"
   license "MIT"
   head "https://github.com/jademind/pi-statusbar.git", branch: "main"
 
--- README.md
+++ README.md
@@ -11,7 +11,7 @@ This repository contains:
 
 ## Version
 
-Latest tagged release: **0.1.7**
+Latest tagged release: **0.1.8**
 
 ---
 
